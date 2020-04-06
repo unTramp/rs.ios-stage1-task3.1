a6285ad977274476dbb5ac2a7da4fd675d253639
unTramp <57003672+unTramp@users.noreply.github.com>
2020-04-06 19:09:51 +0300

    feat: add task3.1

--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -2,6 +2,33 @@
 
 @implementation Combinator
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    
+    NSInteger k = 0;
+    NSInteger m = [array.firstObject integerValue];
+    NSInteger n = [array.lastObject integerValue];
+    
+    NSInteger(^factorial)(NSInteger num);
+    
+    factorial = ^(NSInteger num) {
+        NSInteger number = 1;
+        if (num > 0) {
+            for (NSInteger i = 1; i <= num; i++) {
+                number = i * number;
+            }
+            return number;
+        } else {
+            NSInteger numInt = 1;
+            return numInt;
+        }
+    };
+    
+    while (k < n) {
+        k += 1;
+        NSInteger result = factorial(n) / (factorial(k) * factorial(n - k));
+        if (result >= m) {
+            return [NSNumber numberWithInteger:k];
+        }
+    }
+    return nil;
 }
 @end
